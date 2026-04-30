22e34c90fdf67a291c40edfa2f2865baeedd49c6
ji-circle <sp202401jw@gmail.com>
2026-04-30 15:39:08 +0900

    chore: 주석 제거

--- src/main/java/com/michelet/catalog/domain/repository/ProductViewRepository.java
+++ src/main/java/com/michelet/catalog/domain/repository/ProductViewRepository.java
@@ -6,9 +6,6 @@ import java.util.UUID;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductViewRepository extends MongoRepository<ProductView, String> {
-
-    // @Query 어노테이션 제거함!
-    // Spring Data MongoDB가 'options' 배열 안의 'optionId' 필드를 자동으로 찾아줌
     Optional<ProductView> findByOptionsOptionId(UUID optionId);
 
     Optional<ProductView> findByProductId(UUID productId);
